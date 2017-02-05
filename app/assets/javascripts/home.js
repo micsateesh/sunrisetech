7840743202b2826fc2517292f04f3859de6487ec
micsateesh <mic.sateesh@gmail.com>
2017-02-05 13:29:20 +0530

    sliders added nav header completed

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,32 @@
+// var slideIndex = 1;   manually select
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//     showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     if (n > x.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = x.length} ;
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     x[slideIndex-1].style.display = "block";
+// }
+
+var sindex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    sindex++;
+    if (sindex > x.length) {sindex = 1}
+    x[sindex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
