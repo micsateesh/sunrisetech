316b411c06197e5ee235c2dab829591666e7cdbb
micsateesh <mic.sateesh@gmail.com>
2017-02-12 20:16:43 +0530

    contact completed

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
 source 'https://rubygems.org'
 
-
+#gem 'hpricot'
+gem 'premailer-rails3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+gem 'mail_form'
+gem 'simple_form'
 # Use sqlite3 as the database for Active Record
 group :production do
 	gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207101902) do
+ActiveRecord::Schema.define(version: 20170212104145) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
